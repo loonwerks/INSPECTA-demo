ff74ed6463fd24f272377661cb6d654ff4201927
Alexis Aurandt <Alexis.Aurandt2@collins.com>
2026-02-17 09:20:59 -0500

    Update run_rodeo scripts

--- software/assurance/run_rodeo.sh
+++ software/assurance/run_rodeo.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
 
-pushd $AM_REPOS_ROOT/rust-am-clients
-cargo run --release --bin rust-rodeo-client -- -t $HAMR_ATTESTATION_ROOT/hamr_maestro_term.json -a
-cp ./testing/outputs/appsumm_response.json "$1"
-popd
+rust-rodeo-client -t $HAMR_ATTESTATION_ROOT/hamr_maestro_term.json -s $RODEO_ROOT/rodeo_configs/sessions/session_union.json -m $RODEO_ROOT/testing/manifests/Manifest_P0.json -o $RODEO_ROOT/testing/outputs/ -a
+cp $HAMR_ATTESTATION_ROOT/appsumm_response.json "$1"
